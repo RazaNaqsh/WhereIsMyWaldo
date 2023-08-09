f1a0b96f7e8422af4a4bc5d4bf44fd7162c055a6
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-09 20:05:21 +0530

    Basic structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,22 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <main>
-        <h1>Where's My Waldo</h1>
-      </main>
-    </>
+    <main>
+      <header>
+        <h1 className="text-center text-4xl">Where's My Waldo</h1>
+      </header>
+      <div className="container m-auto h-[80vh] border-2 border-white flex flex-col justify-center items-center">
+        <div className="w-[400px] h-[300px] border-white border-2 "></div>
+        <div className="flex gap-10 mt-6">
+          <div>
+            {" "}
+            <h3>LeaderBoards</h3>
+          </div>
+          <div>
+            <h3>About</h3>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
