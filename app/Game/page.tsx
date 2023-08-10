3c67eef2ee1a32c7ca01d54a1799e0a85da6d14d
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-10 18:17:46 +0530

    Get coordinates when clicked

--- app/Game/page.tsx
+++ app/Game/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import GameHeader from "@/components/GameHeader";
 import Image from "next/image";
 import React from "react";
@@ -5,11 +6,22 @@ import React from "react";
 import waldoImg from "@/public/images/chars.jpg";
 
 const page = () => {
+  const showDetails = (e: any) => {
+    let rect = e.target.getBoundingClientRect();
+    let x = e.clientX - rect.left; //x position within the element.
+    let y = e.clientY - rect.top; //y position within the element.
+    console.log("Left? : " + x + " ; Top? : " + y + ".");
+  };
   return (
     <>
       <GameHeader />
       <div className="container m-auto">
-        <Image src={waldoImg} alt="waldo-image" className="cursor-crosshair" />
+        <Image
+          src={waldoImg}
+          alt="waldo-image"
+          className="cursor-crosshair"
+          onClick={showDetails}
+        />
       </div>
     </>
   );
