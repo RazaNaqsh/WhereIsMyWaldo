877542d2690a19a6ca26e91b23a9e7d109159aa0
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-10 18:06:00 +0530

    Add image and change curosr

--- app/Game/page.tsx
+++ app/Game/page.tsx
@@ -1,10 +1,16 @@
 import GameHeader from "@/components/GameHeader";
+import Image from "next/image";
 import React from "react";
+
+import waldoImg from "@/public/images/chars.jpg";
 
 const page = () => {
   return (
     <>
       <GameHeader />
+      <div className="container m-auto">
+        <Image src={waldoImg} alt="waldo-image" className="cursor-crosshair" />
+      </div>
     </>
   );
 };
