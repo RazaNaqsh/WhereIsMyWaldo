247187be6dcd3c61087f5cca5c494414d8f088ab
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-12 17:37:29 +0530

    Put marker at right place

--- app/Game/page.tsx
+++ app/Game/page.tsx
@@ -1,26 +1,48 @@
 "use client";
 import GameHeader from "@/components/GameHeader";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 import waldoImg from "@/public/images/chars.jpg";
 
 const page = () => {
+  const [marker, setMarker] = useState(null);
+
   const showDetails = (e: any) => {
     let rect = e.target.getBoundingClientRect();
     let x = e.clientX - rect.left; //x position within the element.
     let y = e.clientY - rect.top; //y position within the element.
     console.log("Left? : " + x + " ; Top? : " + y + ".");
+
+    if (!marker) {
+      const newMarker = {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top,
+      };
+
+      setMarker(newMarker);
+    } else {
+      setMarker(null); // Remove the marker on next click
+    }
   };
   return (
     <>
       <GameHeader />
-      <div className="container m-auto">
+      <div
+        className="container m-auto h-full w-full border-2 border-white relative"
+        onClick={showDetails}
+      >
+        {marker && (
+          <div
+            className="marker z-50"
+            style={{ left: marker.x, top: marker.y }}
+          ></div>
+        )}
         <Image
           src={waldoImg}
           alt="waldo-image"
-          className="cursor-crosshair"
-          onClick={showDetails}
+          className="cursor-crosshair -z-0"
+          // onClick={showDetails}
         />
       </div>
     </>
