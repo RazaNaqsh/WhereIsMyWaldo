229ba395fa9aeabdca18f35ce3410926f2066edb
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-17 11:39:22 +0530

    Replace style with tailwind classes for marker

--- app/Game/page.tsx
+++ app/Game/page.tsx
@@ -67,7 +67,7 @@ const page = () => {
       >
         {marker && (
           <div
-            className="marker z-50"
+            className="absolute w-[90px] h-[90px] rounded-full border-[5px] border-dashed border-blue-500 bg-black bg-opacity-40 transform -translate-x-1/2 -translate-y-1/2 z-50"
             style={{ left: marker.x, top: marker.y }}
           ></div>
         )}
