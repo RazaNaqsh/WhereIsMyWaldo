0dfa20791243cff56aa8cda7d0dc59a808debf92
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-20 19:31:00 +0530

    Main screen ui change

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,29 +1,34 @@
 import React from "react";
 import Link from "next/link";
 import { Text } from "@radix-ui/themes";
-
+import Image from "next/image";
+import waldo from "../public/images/chars.jpg";
 const Main = () => {
   return (
     <div className="container m-auto h-[80vh]  flex flex-col justify-center items-center">
-      <div className="w-[400px] h-[300px]  border-white border-2 flex justify-center">
-        <Link
-          href="/Game"
-          className="self-end px-4 py-2 m-2 rounded-md text-white border-2 border-blue-200"
-        >
-          <button>Start</button>
+      <div className="w-[400px] h-[300px] relative  border-white border-2 flex justify-center rounded-md">
+        <Image
+          src={waldo}
+          className="h-full w-full absolute object-cover -z-10 opacity-80"
+          alt="WaldoImage"
+        />
+        <Link href="/Game" className="self-end">
+          <button className="game-btn text-shadow bg-slate-900 hover:bg-slate-800">
+            Start
+          </button>
         </Link>
       </div>
-      <div className="flex gap-10 mt-6">
+      <div className="flex gap-14 mt-8">
         <div>
           {" "}
-          <Text className="text-white" weight="medium" size="5">
-            Leaderboards
-          </Text>
+          <button className="game-btn  text-shadow  bg-indigo-500 hover:bg-indigo-600">
+            LeaderBoards
+          </button>
         </div>
         <div>
-          <Text className="text-white" weight="medium" size="5">
+          <button className="game-btn text-shadow bg-yellow-600 hover:bg-yellow-500">
             About
-          </Text>
+          </button>
         </div>
       </div>
     </div>
