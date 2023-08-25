d066bf61614db9d7ed9d635e407b68f8a03e8f11
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-25 20:06:11 +0530

    show leaderboards

--- app/Leaderboards/page.tsx
+++ app/Leaderboards/page.tsx
@@ -1,10 +1,59 @@
+"use client";
 import Navbar from "@/components/Navbar";
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+interface Score {
+  name: string;
+  id: string;
+  seconds: number;
+}
+
+interface Scores extends Array<Score> {}
 
 const page = () => {
+  const [scores, SetScores] = useState<Scores>();
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`/api/score`);
+      if (response.ok) {
+        const res = await response.json();
+        console.log("Retrieved data:", res.scores);
+        SetScores(res.scores);
+      } else {
+        console.error("Error fetching data");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log(scores);
+  }, []);
+
   return (
     <div>
       <Navbar />
+      <section>
+        <h1 className="text-center ">LeaderBoards</h1>
+        <div>
+          <div className="flex gap-5">
+            <p>Index</p>
+            <p>Name</p>
+            <p>Time Taken</p>
+          </div>
+          {scores &&
+            scores.map((score, idx) => (
+              <div key={score.id} className="flex gap-5">
+                <p>{idx + 1}</p>
+                <p>{score.name}</p>
+                <p>{score.seconds}</p>
+              </div>
+            ))}
+        </div>
+      </section>
     </div>
   );
 };
